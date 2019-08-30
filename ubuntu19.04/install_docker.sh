44600ff35bd6a5ecff22edf48ab1a7ce27dec2a4
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-30 20:58:53 +0800

    升级时删除全部容器

--- ubuntu19.04/install_docker.sh
+++ ubuntu19.04/install_docker.sh
@@ -2,9 +2,12 @@ if ! [ -x "$(command -v docker)" ]; then
     echo "install docker ..."
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
-    apt-get update
-    apt-cache policy docker-ce
-    apt-get install -y docker-ce
+    #apt-cache policy docker-ce
 fi
-
+apt-get update
+# 更新docker
+apt-get install -y docker-ce
+# 删除全部容器
+docker rm -f $(docker ps -aq)
+# 创建docker网络
 docker network create futanaicha
