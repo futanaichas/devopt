d806a2a0c337b01d268143b15aa62dae3173d84c
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-30 18:00:48 +0800

    mysql远程访问

--- ubuntu19.04/install_docker.sh
+++ ubuntu19.04/install_docker.sh
@@ -1,7 +1,10 @@
-echo "install docker ..."
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
-apt-get update
-apt-cache policy docker-ce
-apt-get install -y docker-ce
+if ! [ -x "$(command -v docker)" ]; then
+    echo "install docker ..."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+    apt-get update
+    apt-cache policy docker-ce
+    apt-get install -y docker-ce
+fi
+
 docker network create futanaicha
--- ubuntu19.04/install_mysql.sh
+++ ubuntu19.04/install_mysql.sh
@@ -1,10 +1,12 @@
+# 启动容器
 docker run \
 -p 3306:3306 \
 --name mysql-master \
 --net futanaicha \
 -v ~/devopt/logs:/logs \
 -v ~/devopt/database/mysql:/var/lib/mysql \
+-v ~/devopt/config/mysql/my.cnf:/etc/mysql/my.cnf \
 -e MYSQL_ROOT_PASSWORD=futanaicha \
--d mysql
-
+-d mysql --character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci
 #-v ~/devopt/config/mysql:/etc/mysql/conf.d \
+
