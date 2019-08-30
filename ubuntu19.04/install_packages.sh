7f6525177d230749138c9f2e88f81d772449b441
Concurrent <C.Y.Concurrent@gmail.com>
2019-08-31 00:54:10 +0800

    添加子模块

--- ubuntu19.04/install_packages.sh
+++ ubuntu19.04/install_packages.sh
@@ -1,8 +1,8 @@
+echo "install packages"
 root_path=$(pwd)
 function install_static_service(){
     echo "install_static_service"
     cd $root_path/packages/static-service
     bash ./install.sh
 }
-
 install_static_service
